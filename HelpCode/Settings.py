73fb674975d24e10f223ecb47f78499138179c90
Superkingpat <62884313+Superkingpat@users.noreply.github.com>
2024-03-11 14:05:35 +0100

    TSR-3 prometni znaki Unsorted / image resized code

---
+++ HelpCode/Settings.py
@@ -0,0 +1,19 @@
+###Settings###
+picDirectory = "C:/Users/patri/Desktop/Delo/Urejanje slik/I" #You need to put in on which port is external drive OR if you leave it empty and put program in folder with it
+overWrite = False #Program is going to overwrite pictures in new format you might have problem if you dont have enough space on external drive 
+picOutputDirectory = "C:/Users/patri/Desktop/Delo/Urejanje slik/O" #Pictures will go in that folder in case that overWrite = False 
+nameOfBlockFile = "/blockList.txt" #Files you don't want to show //Insert only name without format
+nameOfTimeSortedFile = "/newToOldSorting.txt" #name of txt file
+nameOfHelpFile = "/helpingFile.txt" #name of helping file
+nameOfHelpFile2 = "/helpingFile2.txt" #name of helping file
+nameOfPicHelpFile = "/picHelpingFile.txt" #name of helping file
+#Picture
+color = "white" #white or black
+wantedSizeX = 800
+wantedSizeY = 480
+pictureFormat = ".bmp"
+#Showcase
+frameInterval = "s" #week, day, hour, minute, second // you can also use first letters
+frameRate = 1 #Rate
+order = 1 #1 = old to new/-1 = new to old
+##############
