a4c719a5a532fbcec09eac8fed3f2ace1c60aa7e
Pat <62884313+Superkingpat@users.noreply.github.com>
2024-03-22 20:38:20 +0100

    Ai model Medium

--- AI/YOLOv8_version_0.2/load_model.py
+++ AI/YOLOv8_version_0.2/load_model.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
+import cv2
 
-model = YOLO(r'runs\detect\yolov8n_tsd\weights\best.pt')
+if __name__ == '__main__':
+    model = YOLO(r"C:\Users\patri\OneDrive\Dokumenti\GITHUB\TSR-AI_Traffic_Sign_Recognition\runs\detect\yolov8n_tsd12\weights\best.pt")
 
-result = model(r'C:\Data\lableimg\JPEGImages\JPEGImages\0000654.jpg')
+    result = model.predict(r"C:\Users\patri\Downloads\20240321_135051.jpg")
 
-result[0].show()
+    result[0].show()
+
