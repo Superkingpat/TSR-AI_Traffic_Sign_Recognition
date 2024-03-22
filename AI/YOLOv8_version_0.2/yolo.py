ba69179faea73ace983e16486fa4cf807276fe94
Pat <62884313+Superkingpat@users.noreply.github.com>
2024-03-22 20:38:20 +0100

    TSR-14 AI model Medium

--- AI/YOLOv8_version_0.2/yolo.py
+++ AI/YOLOv8_version_0.2/yolo.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 import cv2
+if __name__ == '__main__':
+    model = YOLO('yolov8m.pt')
 
-model = YOLO('yolov8s.pt')
-
-results = model.train(
-    data='DtSt\data.yaml',
-    imgsz=[1280, 720],
-    epochs=25,
-    batch=16,
-    name='yolov8n_tsd',
-    rect=True
-)
+    results = model.train(
+        data=r'C:\Users\patri\OneDrive\Dokumenti\GitHub\TSR-AI_Traffic_Sign_Recognition\AI\YOLOv8_version_0.2\DtSt\data.yaml',
+        imgsz=[1280, 720],
+        epochs=25,
+        batch=16,
+        name='yolov8n_tsd',
+        rect=True
+    )
