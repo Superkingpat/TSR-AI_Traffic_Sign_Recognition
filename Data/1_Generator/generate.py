599bedfa1a4c9d19a71e3fb95947c3c79215b781
Superkingpat <62884313+Superkingpat@users.noreply.github.com>
2024-03-29 23:39:42 +0100

    Added custom plsql support / setting validation

--- Data/1_Generator/generate.py
+++ Data/1_Generator/generate.py
@@ -1,5 +1,37 @@
 from settings import *
 from PIL import Image
+from DBManager import execute
+import Augmentor as ag
+import numpy as np
+
+def validateSettings():
+    print_string = "ERROR: Variable is not correct type:"
+    if type(ONLY_ENVIRONMENTS) not in [type(None), type([])]:
+        print(print_string, "ONLY_ENVIRONMENTS || ", type(ONLY_ENVIRONMENTS))
+    if type(IGNORE_ENVIRONMENTS) not in [type(None), type([])]:
+        print(print_string, "ONLY_ENVIRONMENTS || ", type(IGNORE_ENVIRONMENTS))
+    if type(IGNORE_TAGS) not in [type(None), type([])]:
+        print(print_string, "ONLY_ENVIRONMENTS || ", type(IGNORE_TAGS))
+    if type(ONLY_TAGS) not in [type(None), type([])]:
+        print(print_string, "ONLY_ENVIRONMENTS || ", type(ONLY_TAGS))
+
+def augmentImage(image):
+    image_directory = r"C:\Data\lableimg\TF_images\predict\crops\Stop"
+    output_directory = r"C:\Data\lableimg\TF_images\predict\crops\aug_Stop"
+    aug = ag.Pipeline(image_directory, output_directory=output_directory, save_format="JPEG")
+    aug.skew(probability=0.7, magnitude=0.7)
+    aug.random_distortion(probability=0.6, magnitude=2, grid_width=6, grid_height=6)
+    aug.rotate(probability=0.6, max_left_rotation=25, max_right_rotation=25)
+    aug.resize(probability=1, width=140, height=140)
+    aug.sample(100)
+
+def select_environments():
+    if ONLY_ENVIRONMENTS != None:
+        return ONLY_ENVIRONMENTS
+    else:
+        dict = execute(":out := SELECT fileName FROM EnvConfig")
+        items = np.array(dict[":out"]).flatten()
+    sql_script = ""
 
 def load_environment_image(image_path):
     try:
@@ -14,7 +46,10 @@ def generate_images():
     pass
 
 def workload_manager():
-    pass
+    #image = load_environment_image()
+    pass 
 
 if __name__ == "__main__":
-    load_environment_image(DIR+"/test_env.jpg")
+    validateSettings()
+    select_environments()
+    #workload_manager()
