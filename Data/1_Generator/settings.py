ac2c358ad48604e34e72a9d636c0b280ac1ad988
Superkingpat <62884313+Superkingpat@users.noreply.github.com>
2024-03-29 23:39:42 +0100

    Added custom plsql support / setting validation

--- Data/1_Generator/settings.py
+++ Data/1_Generator/settings.py
@@ -2,9 +2,9 @@ from os.path import abspath, dirname, join
 DIR = dirname(abspath(__file__))
 
 DATABASE_NAME = "EnvConfig.db"
-PATH_TO_SIGN_FOLDER = r"C:\Users\steam\OneDrive\Namizje\test"
+PATH_TO_SIGN_FOLDER = join(DIR, "")
 PATH_TO_ENV_FOLDER = join(DIR, "Environment")
-PATH_TO_GEN_FOLDER = ""
+PATH_TO_GEN_FOLDER = join(DIR, "")
 
 OUTPUT_RESOLUTION = None
 
@@ -19,4 +19,9 @@ NUM_OF_SIGNS = (1,5)
 ROTATION_2D = (-20,20)
 ROTATION_3D = (-20,20)
 
+# ENVIRONMENT SETTINGS
+ONLY_ENVIRONMENTS = None
 IGNORE_ENVIRONMENTS = None
+
+ONLY_TAGS = None
+IGNORE_TAGS = None
