acfcd5a155dea570418418e565f3095ac59837af
Superkingpat <62884313+Superkingpat@users.noreply.github.com>
2024-04-19 19:48:34 +0200

    TSR-27 setting update

--- Data/1_Generator/settings.py
+++ Data/1_Generator/settings.py
@@ -8,17 +8,19 @@ PATH_TO_SIGN_FOLDER = "C:\\Users\\steam\\OneDrive\\Namizje\\test"
 PATH_TO_SIGN_FOLDER = join(DIR, "")
 >>>>>>> 599bedf (Added custom plsql support / setting validation)
 PATH_TO_ENV_FOLDER = join(DIR, "Environment")
-PATH_TO_GEN_FOLDER = join(DIR, "")
+PATH_TO_GEN_FOLDER = join(DIR, "Generated")
 
 OUTPUT_RESOLUTION = None
 
 NUM_OF_GEN_PHOTOS = 10
 
+
+# AUGMENTATION SETTINGS
 CORRECT_COLORS = False
 LABEL_PHOTOS = False
 SAVE_GEN_LOG = False
 
-# MIN - MAX limits
+## MIN - MAX limits
 NUM_OF_SIGNS = (1,5)
 ROTATION_2D = (-20,20)
 ROTATION_3D = (-20,20)
@@ -26,6 +28,11 @@ ROTATION_3D = (-20,20)
 # ENVIRONMENT SETTINGS
 ONLY_ENVIRONMENTS = None
 IGNORE_ENVIRONMENTS = None
+ONLY_ENV_TAGS = None
+IGNORE_ENV_TAGS = None
 
-ONLY_TAGS = None
-IGNORE_TAGS = None
+# SIGNS SETTINGS
+ONLY_SIGNS = None
+IGNORE_SIGNS = None
+ONLY_SIG_TAGS = None
+IGNORE_SIG_TAGS = None
