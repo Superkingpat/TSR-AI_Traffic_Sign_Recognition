9c62d0b248f48a16a530a150883c9efef7887f6b
Pat <62884313+Superkingpat@users.noreply.github.com>
2024-05-03 22:39:40 +0200

    TSR-36 Video to Frames

---
+++ Data/3_Video_To_Jpg/main.py
@@ -0,0 +1,22 @@
+from os import listdir, system, makedirs, remove
+from os.path import join, abspath, exists, dirname
+
+#####    REQUIRES DATA_IN FOLDER !!!!!    #####
+#####    REQUIRES FFMPEG !!!!!    ##### 
+# https://univerzamb-my.sharepoint.com/:u:/g/personal/patrik_gobec_student_um_si/EWyX-xlH5o1Ds6qb2Fl_38sB4lSGrW9rPGnt4JGOqJ1q_w?e=mkjKI4
+
+def create_dir(path):
+    try:
+        if not exists(path):
+            makedirs(path)
+    except OSError:
+        print ('Error: Creating directory. ' +  path)
+
+if __name__ == "__main__":
+    data_in = join(dirname(abspath(__file__)), "data_IN")
+    data_out = join(dirname(abspath(__file__)), "data_OUT")
+    create_dir(data_out)
+    videos = [f for f in listdir(data_in) if f.endswith(".mp4") or f.endswith(".MP4")] # Compatible with mp4 (extend code with caution)
+
+    for video in videos:
+        system(f"{dirname(abspath(__file__))}/ffmpeg -i {join(data_in,video)} -vf fps=1/10 {data_out}/{video.split(".")[0]}_%d.jpg")
