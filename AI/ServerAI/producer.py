8d2c13517bc4cdb72965f6cd368d8f62fe636b85
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-05-04 16:00:59 +0200

    TSR-69 Producer sends an image every 15 seconds

--- AI/ServerAI/producer.py
+++ AI/ServerAI/producer.py
@@ -1,7 +1,10 @@
 from confluent_kafka import Producer
 import time
+import cv2
+import numpy as np
 
-ip = '10.8.2.3:9092'
+#ip = '10.8.2.3:9092'
+ip = 'localhost:9092'
 
 p = Producer({'bootstrap.servers': ip})
 
@@ -9,14 +12,14 @@ def delivery_report(err, msg):
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
-
-i = 0
+        print('Message delivered')
 
 while True:
     p.poll(0)
-    i += 1
-    p.produce('mytopic', str(i).encode('utf-8'), callback=delivery_report)
-    time.sleep(2)
+    image = cv2.imread(r'C:\Users\steam\OneDrive\Namizje\TF_Signs\Dataset\Round\30\0000056.jpg')
+    ret, buffer = cv2.imencode('.jpg', image)
+
+    p.produce('mytopic', value=buffer.tobytes(), callback=delivery_report)
+    time.sleep(15)
 
 p.flush()
