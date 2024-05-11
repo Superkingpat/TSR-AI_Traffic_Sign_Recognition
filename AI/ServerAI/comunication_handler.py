3f8fbb9689fdf50c3068653b22a08089168760de
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-05-11 18:11:03 +0200

    Added produce consume and end_handler functions

--- AI/ServerAI/comunication_handler.py
+++ AI/ServerAI/comunication_handler.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, Consumer, KafkaException
+from confluent_kafka import Producer, Consumer, error
 from confluent_kafka.admin import AdminClient, NewTopic
 import tensorflow as tf
 import numpy as np
@@ -7,13 +7,14 @@ from io import BytesIO
 import cv2
 
 class comunicationHandler:
-    def __init__(self, server_address, group_name):
+    def __init__(self, server_address, consumer_group_name):
         self.server_address = server_address
-        self.group_name = group_name
+        self.consumer_group_name = consumer_group_name
         self.admin_client = AdminClient({'bootstrap.servers': self.server_address})
+
         self.consumer = Consumer({
             'bootstrap.servers': server_address,
-            'group.id': group_name,
+            'group.id': consumer_group_name,
             'auto.offset.reset': 'earliest'
         })
 
@@ -30,11 +31,15 @@ class comunicationHandler:
                 print(f"Topic {future.topic} created with replication factor {future.replication_factor}")
             except Exception as e:
                 raise(f"Failed to create topic {future.topic}: {e}")
+            
+    def __delivery_report(err, msg):
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
         
     def set_consumer_topic_subscribtion(self, topic, auto_topic_creation=True, num_partitions=1, replication_factor=1):
         try:
             self.consumer.subscribe([topic])
-        except KafkaException as e:
+        except error.KafkaException as e:
             if 'Unknown topic or partition' in str(e):
                 if auto_topic_creation:
                     self.create_topic(topic, num_partitions, replication_factor)
@@ -43,3 +48,23 @@ class comunicationHandler:
             else:
                 raise(f"An error occures. Error: {str(e)}")
     
+    def produce(self, topic, massage):
+        try:
+            self.producer.produce(topic, value=massage, callback=self.__delivery_report)
+            #self.producer.flush()
+        except error.KafkaError as e:
+            print(f"Kafka error occured: {str(e)}")
+    
+    def consume(self, timeout_len=1.0):
+        consumer_msg = None
+        try:
+            consumer_msg = self.consumer.poll(timeout_len)
+            if consumer_msg is None:
+                print(f"Timeout reached. Timeout was set to {timeout_len} seconds.")
+            else:
+                return consumer_msg
+        except error.KafkaError as e:
+            raise(f"Kafka error occured: {str(e)}")
+    
+    def end_handler(self):
+        self.consume.close()
