38ff8b3ae86dda0a3dd2324e70fd4f950e3b10bc
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-05-11 17:40:27 +0200

    Added create_topic and set_consumer_topic_subscription

--- AI/ServerAI/comunication_handler.py
+++ AI/ServerAI/comunication_handler.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, Consumer
+from confluent_kafka import Producer, Consumer, KafkaException
 from confluent_kafka.admin import AdminClient, NewTopic
 import tensorflow as tf
 import numpy as np
@@ -20,5 +20,26 @@ class comunicationHandler:
         self.producer = Producer({'bootstrap.servers': server_address})
 
 
-    def create_topic():
-        pass
+    def create_topic(self, topic_name, num_partitions, replication_factor):
+        topic_conf = NewTopic(topic_name, num_partitions, replication_factor)
+        futures = self.admin_client.create_topics([topic_conf])
+
+        for future in futures:
+            try:
+                future.result()
+                print(f"Topic {future.topic} created with replication factor {future.replication_factor}")
+            except Exception as e:
+                raise(f"Failed to create topic {future.topic}: {e}")
+        
+    def set_consumer_topic_subscribtion(self, topic, auto_topic_creation=True, num_partitions=1, replication_factor=1):
+        try:
+            self.consumer.subscribe([topic])
+        except KafkaException as e:
+            if 'Unknown topic or partition' in str(e):
+                if auto_topic_creation:
+                    self.create_topic(topic, num_partitions, replication_factor)
+                else:
+                    raise(f"Topic {topic} does not exist. auto_topic_creation set to false.")
+            else:
+                raise(f"An error occures. Error: {str(e)}")
+    
