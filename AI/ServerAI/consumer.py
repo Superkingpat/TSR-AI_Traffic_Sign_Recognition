fc74fdf3a4bf7017f7145a3032be0006e12b2298
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-05-14 14:14:52 +0200

    Bi-directional comunication

--- AI/ServerAI/consumer.py
+++ AI/ServerAI/consumer.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 import cv2
+from comunication_handler import comunicationHandler
 
 def preprocess_image(image_bytes):
     image = Image.open(BytesIO(image_bytes))
@@ -12,12 +13,28 @@ def preprocess_image(image_bytes):
     image_array = image_array / 255.0
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
+
 model = tf.keras.models.load_model('speed_classification.h5')
 class_ind = {'100': 0, '130': 1, '30': 2, '40': 3, '40-': 4, '50': 5, '50-': 6, '60': 7, '60-': 8, '70': 9, '70-': 10, '80': 11, '90': 12}
 class_ind = {v: k for k, v in class_ind.items()}
 
 ip = 'localhost:9092'
 
+handle = comunicationHandler(ip, 'server_group')
+handle.set_consumer_topic_subscribtion('from_client')
+
+while True:
+    msg = handle.consume(1.0)
+
+    if msg is not None:
+        stream = BytesIO(msg.value())
+        image = Image.open(stream).convert("RGB")
+        image_array = preprocess_image(stream.getvalue())
+        pred = model.predict(image_array)
+        pred = bytes(class_ind[np.argmax(pred)], 'utf-8')
+        handle.produce('to_client', pred)
+
+'''
 c = Consumer({
     'bootstrap.servers': ip,
     'group.id': 'mygroup',
@@ -43,3 +60,4 @@ while True:
     #image.save('received_image.png')
 
 c.close()
+'''
