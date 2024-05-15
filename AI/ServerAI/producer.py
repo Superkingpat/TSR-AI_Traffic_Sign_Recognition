ce138b134153e46c400be30d49eed41e51605c35
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-05-15 13:48:11 +0200

    Some changes and checks. Delay test

--- AI/ServerAI/producer.py
+++ AI/ServerAI/producer.py
@@ -4,6 +4,7 @@ import cv2
 from io import BytesIO
 import numpy as np
 from comunication_handler import comunicationHandler
+from time import time
 
 #ip = '10.8.2.3:9092'
 ip = 'localhost:9092'
@@ -16,28 +17,8 @@ while True:
     ret, buffer = cv2.imencode('.jpg', image)
 
     handle.produce('from_client', buffer.tobytes())
-    msg = handle.consume(1.0)
+    msg = handle.consume(0.1)
+    tim = time()
     if msg is not None:
-        print(BytesIO(msg.value()))
-    time.sleep(25)
-
-
-'''
-p = Producer({'bootstrap.servers': ip})
-
-def delivery_report(err, msg):
-    if err is not None:
-        print('Message delivery failed: {}'.format(err))
-    else:
-        print('Message delivered')
-
-while True:
-    p.poll(0)
-    image = cv2.imread(r'C:\Users\steam\OneDrive\Namizje\TF_Signs\Dataset\Round\30\0000056.jpg')
-    ret, buffer = cv2.imencode('.jpg', image)
-
-    p.produce('img_stream', value=buffer.tobytes(), callback=delivery_report)
-    time.sleep(25)
-
-p.flush()
-'''
+        print(f"{msg.value()} {tim}")
+    #time.sleep(5)
