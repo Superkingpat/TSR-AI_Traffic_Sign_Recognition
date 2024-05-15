4ecbecee3ef49c6b8854741658fb81857507155b
Superkingpat <62884313+Superkingpat@users.noreply.github.com>
2024-05-15 12:00:50 +0200

    TSR-48 labeling corrections

--- AI/YOLOv8_version_0.2/load_model.py
+++ AI/YOLOv8_version_0.2/load_model.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 if __name__ == '__main__':
-    model = YOLO(r"C:\Projects\AI_TSR\TSR-AI_Traffic_Sign_Recognition\AI\YOLOv8_version_0.2\runs\detect\yolov8n_tsd12\weights\best.pt")
+    model = YOLO(r"C:\Users\patri\Dokumenti\GITHUB\TSR-AI_Traffic_Sign_Recognition\runs\detect\yolov8n_tsd12\weights\best.pt")
 
     result = model.predict(r"C:\Projects\AI_TSR\TSR-AI_Traffic_Sign_Recognition\AI\ServerAI\1716116788.0306077.jpg")
 
--- AI/YOLOv8_version_0.2/yolo.py
+++ AI/YOLOv8_version_0.2/yolo.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 import cv2
 if __name__ == '__main__':
-    model = YOLO('yolov8m.pt')
+    model = YOLO('yolov8n.pt')
 
     results = model.train(
-        data=r'C:\Users\patri\OneDrive\Dokumenti\GitHub\TSR-AI_Traffic_Sign_Recognition\AI\YOLOv8_version_0.2\DtSt\data.yaml',
+        data=r'C:\Users\patri\Documents\GitHub\TSR-AI_Traffic_Sign_Recognition\AI\YOLOv8_version_0.2\DtSt\data.yaml',
         imgsz=[1280, 720],
         epochs=25,
-        batch=16,
+        batch=4,
         name='yolov8n_tsd',
         rect=True
     )
