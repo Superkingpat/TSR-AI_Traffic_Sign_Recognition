a113bb7e3c5c4a1279d4e23bd544b027765c204c
Pat <62884313+Superkingpat@users.noreply.github.com>
2024-05-18 19:23:08 +0200

    yolo Update CUDA

--- AI/YOLOv8_version_0.2/yolo.py
+++ AI/YOLOv8_version_0.2/yolo.py
@@ -1,14 +1,13 @@
 from ultralytics import YOLO
 import cv2
 if __name__ == '__main__':
-    model = YOLO('yolov8s.pt')
+    model = YOLO(r"C:\Users\patri\OneDrive\Dokumenti\GITHUB\TSR-AI_Traffic_Sign_Recognition\runs\detect\yolov8l_tsd_100K\weights\best.pt")
     model.to('cuda')
 
     results = model.train(
-        data=r'C:\Users\patri\OneDrive\Dokumenti\GITHUB\TSR-AI_Traffic_Sign_Recognition\dataset\data.yaml',
-        imgsz= 1080, #[1280, 720],
+        data=r"C:\Users\patri\OneDrive\Dokumenti\GITHUB\TSR-AI_Traffic_Sign_Recognition\dataset\data.yaml",
+        #imgsz= 1080, #[1280, 720],
         epochs=1000,
         batch=16,
-        name='yolov8s_tsd',
-        single_cls=True,
+        name='yolov8l_tsd_100K'
     )
