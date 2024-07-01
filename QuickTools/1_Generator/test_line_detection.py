bdee13f5551246521833dfa8fabf345be763d632
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-07-01 18:11:07 +0200

    TSR-67 Github action line detection test

---
+++ QuickTools/1_Generator/test_line_detection.py
@@ -0,0 +1,19 @@
+from line_detection import load_image, get_random_position, get_n_random_positions
+
+def test_load_image():
+    test_image_path = 'test_env.jpg'
+    load_image(test_image_path)
+    print("load_image function executed successfully.")
+
+def test_get_random_position():
+    position = get_random_position()
+    print(f"Random position: {position}")
+
+def test_get_n_random_positions():
+    positions = get_n_random_positions(5)
+    print(f"Random positions: {positions}")
+
+if __name__ == "__main__":
+    test_load_image()
+    test_get_random_position()
+    test_get_n_random_positions()
