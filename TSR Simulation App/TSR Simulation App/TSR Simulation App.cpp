bdadc07daad19abc955c6c276e6a7d4fb8171a22
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-12-27 21:20:17 +0100

    Added Debug to .gitignore

---
+++ TSR Simulation App/TSR Simulation App/TSR Simulation App.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <map>
+#include <glm.hpp>
+#include <matrix_transform.hpp>
+#include <type_ptr.hpp>
+#include <memory>
+
+
+int main() {
+	return 0;
+}
