073d83a96a469e162ecf62842da926b350336fca
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2024-12-29 17:47:37 +0100

    Made declarations of the necceserry functions and variables

--- TSR Simulation App/TSR Simulation App/TSR_Simulation.h
+++ TSR Simulation App/TSR Simulation App/TSR_Simulation.h
@@ -1,5 +1,35 @@
 #pragma once
-class TSR_Simulation
-{
-};
 
+#include "ObjectHandler.h"
+
+class TSR_Simulation {
+private:
+	uint32_t M_SCR_WIDTH;
+	uint32_t M_SCR_HEIGHT;
+
+	GLFWwindow* m_window = nullptr;
+
+	ShaderHandler m_shaderHandler;
+
+	ObjectHandler m_objectHandler;
+
+	CameraHandler m_cameraHandler;
+
+	void Init();
+	void InitCamera();
+	void InitGLFW();
+	void InitOpenGL();
+	void InitRenderObjects();
+	void InitBuffers();
+	void InitShaders();
+
+	void Update();
+
+	void Draw();
+	void DrawCubemap();
+public:
+
+	TSR_Simulation();
+
+	int Run();
+};
