ffde906de1b1c09a85a3df374fe1c5913783e2ff
Pat <62884313+Superkingpat@users.noreply.github.com>
2025-01-21 12:34:19 +0100

    rename

--- ServerSim/consumer.py
+++ ServerSim/consumer.py
@@ -38,8 +38,8 @@ CONFIDENCE_THRESHOLD = 0.80
 
 start_http_server(8000)
 
-model_tf = tf.keras.models.load_model('TSC_final_DN121.h5')
-model_yolo = YOLO('best.pt')
+model_tf = tf.keras.models.load_model('TSC_simulation_DN121.h5')
+model_yolo = YOLO('bestSIM.pt')
 
 class_index = {'10': 0, '100': 1, '120': 2, '20': 3, '30': 4, '30-': 5, '40': 6, '40-': 7, '50': 8, '50-': 9, '60': 10, '60-': 11, '70': 12, '70-': 13, '80': 14, '80-': 15, 'delo_na_cestiscu': 16, 'kolesarji_na_cestiscu': 17, 'konec_omejitev': 18, 'odvzem_prednosti': 19, 'otroci_na_cestiscu': 20, 'prednost': 21, 'prehod_za_pesce': 22, 'stop': 23, 'unknown': 24}
 class_index = {v: k for k, v in class_index.items()}
